3c068fdcf8bb8463c7085573a9f0ae97593f5fd1
Sergey Margelov <smargelov@gmail.com>
2022-07-06 02:37:56 +0400

    Add message for commands

--- src/index.js
+++ src/index.js
@@ -10,6 +10,34 @@ dotenv.config()
 const token = process.env.TELEGRAM_TOKEN
 const bot = new TelegramBot(token, {polling: true})
 
+bot.onText(/\/start/, (msg) => {
+    bot.sendMessage(msg.chat.id, config.messages.ABOUT)
+})
+
+bot.onText(/\/develop/, (msg) => {
+    bot.sendMessage(msg.chat.id, config.messages.DEVELOP, {parse_mode: 'Markdown', disable_web_page_preview: true})
+})
+
+bot.onText(/\/commands/, (msg) => {
+    bot.sendMessage(msg.chat.id, config.messages.COMMANDS)
+})
+
+bot.onText(/\/statistics/, (msg) => {
+    bot.sendMessage(msg.chat.id, config.messages.STATISTICS)
+})
+
+bot.onText(/\/author/, (msg) => {
+    bot.sendMessage(msg.chat.id, config.messages.AUTHOR)
+})
+
+bot.onText(/\/privacy/, (msg) => {
+    bot.sendMessage(msg.chat.id, config.messages.PRIVACY)
+})
+
+bot.onText(/\/donation/, (msg) => {
+    bot.sendMessage(msg.chat.id, config.messages.DONATION)
+})
+
 bot.on('voice', async (msg) => {
     const messageId = msg.message_id
     const isForwarded = !!msg.forward_from
