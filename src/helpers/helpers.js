1bdc465c1451c8f6ff53747d1be88c7caa5398cc
Sergey Margelov <smargelov@gmail.com>
2022-07-10 15:47:25 +0400

    Add tests for download function

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -2,23 +2,25 @@ const request = require('request')
 const fs = require('fs')
 
 const download = (url, path, callback) => {
-	request.head(url, (err, res, body) => {
-		request(url).pipe(fs.createWriteStream(path)).on('close', callback);
-	})
+    request.head(url, (err, res, body) => {
+        if (err && res.statusCode !== 200) {
+            throw new Error('Error downloading file')
+        }
+        request(url).pipe(fs.createWriteStream(path)).on('close', callback)
+    })
 }
 
 const removeFile = async (path) => {
-	try	{
-		await fs.unlink(path, (err) => {
-			if (err) throw err
-		})
-	}
-	catch (e) {
-		console.log(e)
-	}
+    try {
+        await fs.unlink(path, (err) => {
+            if (err) throw err
+        })
+    } catch (e) {
+        console.log(e)
+    }
 }
 
 module.exports = {
-	download,
-	removeFile
+    download,
+    removeFile
 }
---
+++ src/helpers/helpers.spec.js
@@ -0,0 +1,29 @@
+const {download} = require('./helpers')
+const fs = require('fs')
+
+describe('Function "download"', () => {
+    const testDirPath = 'test'
+    beforeEach(() => {
+        fs.mkdirSync(testDirPath)
+    })
+    afterEach(() => {
+        fs.rm(testDirPath, {recursive: true})
+    })
+    test('should download file', async () => {
+        const url = 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png'
+        const path = `${testDirPath}/test.png`
+        const callback = () => {
+            expect(fs.readdirSync(testDirPath)).toContain('test.png')
+            expect(fs.existsSync(path)).toBe(true)
+        }
+        await download(url, path, callback)
+    })
+    test('should throw error', async () => {
+        const url = 'https://www.google.com/images/wrong-path.png'
+        const path = `${testDirPath}/test.png`
+        const callback = jest.fn()
+        await download(url, path, callback)
+        expect(callback).not.toHaveBeenCalled()
+        expect(fs.existsSync(path)).toBe(false)
+    })
+})
